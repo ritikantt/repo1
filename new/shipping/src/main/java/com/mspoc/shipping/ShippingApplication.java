7dba9e25d7dfa8fb83af613c22a87dcb52d23f62
RitikaSaini <ritikasony08@gmail.com>
2023-03-30 18:39:38 +0530

    autwired added

--- new/shipping/src/main/java/com/mspoc/shipping/ShippingApplication.java
+++ new/shipping/src/main/java/com/mspoc/shipping/ShippingApplication.java
@@ -1,5 +1,6 @@
 package com.mspoc.shipping;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -24,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ShippingApplication {
 
+  @Autowired
   private ShippingService shippingService;
   
   public static void main(String[] args) {
